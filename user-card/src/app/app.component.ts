ba789d05b695b7deb63f56d4d9b9953db5376960
LUCA\lucas <lucasguera2@outlook.fr>
2025-03-27 10:56:36 +0100

    Clean de app.component.html, mise en place d'un premier 'style' simple pour acceuillir le premier component dans app.component.scss, création du premier component à l'aide de la commande" ng g c components/user-card"

--- user-card/src/app/app.component.ts
+++ user-card/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
