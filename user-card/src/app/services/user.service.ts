090bfdc9f02b2711391df279f31eccfdcde98d52
LUCA\lucas <lucasguera2@outlook.fr>
2025-03-27 12:39:24 +0100

    Modification du service afin de catcher s'il y a une erreur lors du call(interuption d'api ...)

--- user-card/src/app/services/user.service.ts
+++ user-card/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -25,13 +25,22 @@ export class UserService {
       // Quand on reçoit la réponse, on utilise .map de la librairie rxjs
       map((response: { results: User[] }) =>
         // On extrait le premier user du tableau
-        response.results[0])
-    );
+        response.results[0]),
+        // En cas d'erreur lors de l'appel HTTP
+        catchError(error => {
+          console.error(`Erreur lors du cahrgement d'un utilisateur`, error);
+          return throwError(() => new Error(`Impossible de charger l'utilisateur`))
+        })
+    )
   }
 
   getListofRandomUsers(count: number = 10): Observable<User[]> {
     return this.http.get<{ results: User[] }>(`${this.url_api_user}?results=${count}`).pipe(
-      map((response: { results: User[] }) => response.results)
+      map((response: { results: User[] }) => response.results),
+      catchError(error => {
+        console.error(`Erreur lors du cahrgement de la liste d'utilisateur`, error);
+        return throwError(() => new Error(`Impossible de charger la liste d'utilisateur`));
+      })
     );
   }
 
