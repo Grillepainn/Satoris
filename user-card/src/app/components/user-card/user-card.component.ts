fda551dc8e51d343129eaad15f750f6fe4f4c65c
LUCA\lucas <lucasguera2@outlook.fr>
2025-03-27 11:37:10 +0100

    Correctif en remplaçant HttpClientModule par la méthode recommandée `provideHttpClient()` dans le fichier main.ts ( lié à l'utilisation d'un composant standalone), création de la 1ere card et mise en forme de cette dernière

--- user-card/src/app/components/user-card/user-card.component.ts
+++ user-card/src/app/components/user-card/user-card.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
+
 export class UserCardComponent {
+  user:any;
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getRandomUser().subscribe(res => {
+      this.user = res.results[0];
+    })
+  }
 
 }
